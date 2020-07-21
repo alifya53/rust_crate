ad34fe832cdf9ae06b8b0c06c1852b5110dd0dd8
alifya53 <alifyahussian53@gmail.com>
2020-07-21 17:00:35 +0500

    initial commit

---
+++ input_crate/target/rls/debug/deps/input_crate-258101e6560a05fc.d
@@ -0,0 +1,5 @@
+/home/alifya/Documents/cratesio_lib/input_crate/target/rls/debug/deps/input_crate-258101e6560a05fc.rmeta: src/lib.rs
+
+/home/alifya/Documents/cratesio_lib/input_crate/target/rls/debug/deps/input_crate-258101e6560a05fc.d: src/lib.rs
+
+src/lib.rs:
